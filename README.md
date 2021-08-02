b3e6a4615046718c7b9bebd481956648d7ea33cb
Benjamin <46236611+notquiteprogrammer@users.noreply.github.com>
2021-08-01 23:17:53 -0400

    Began work on checking assets
    wrote pseudocode for checking assets

---
+++ README.md
@@ -0,0 +1,1 @@
+ok but like where's rasazy
---
+++ source/CheckAssets.hx
@@ -0,0 +1,10 @@
+package source;
+import haxe.crypto;
+
+
+//for image in files:
+//  convert file to binary/hexadecimal
+//  generate MD5 checksum using haxe.crypto
+//  compare MD5 checksum w/ precalculated checksum
+//      if no match, then activate protocol Selever
+//      if missing, then (nightmare fuel??????)
